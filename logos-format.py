e0a9f818e231524e39d699d2ad10432da909f794
HearseDev <admin@Junes-Air.hsd1.ca.comcast.net>
2021-12-07 13:57:41 -0800

    rewrote logic-thanks to uroboro

--- logos-format.py
+++ logos-format.py
@@ -3,29 +3,23 @@ from subprocess import Popen, PIPE, STDOUT
 
 filterList = ["%hook","%end","%log","%orig","%class","%new","%init", "%ctor","%group","%subclass"]
 
-
 fileContentsList = sys.stdin.read().splitlines()
 
 newList = []
-skipLine = False
 
 for line in fileContentsList:
-    for filter in filterList:
-        if line.find(filter) != -1:
+    if any(token in line for token in filterList):
             newList.append("/* clang-format off */")
             newList.append(line)
             newList.append("/* clang-format on */")
-            skipLine = True;
-    if skipLine == False:
-      newList.append(line);
-    skipLine = False;
+    else:
+            newList.append(line);
 
 command = ["clang-format"] + sys.argv[1:]
 process = Popen(command, stdout=PIPE, stderr=None, stdin=PIPE)
 stdoutData = process.communicate(input= "\n".join(newList).encode())[0]
 
 refinedArr = stdoutData.decode().splitlines()
-str = ""
 for line in refinedArr:
-    if line.find("/* clang-format off */") == -1 and line.find("/* clang-format on */") == -1:
+    if "/* clang-format off */" not in line and "/* clang-format on */" not in line:
         print(line)
