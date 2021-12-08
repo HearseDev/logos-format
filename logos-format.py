29ecd84142e2942dc798a0654648e5740ab7e769
HearseDev <hearsd@gmail.com>
2021-12-08 12:35:50 -0800

    format using black

--- logos-format.py
+++ logos-format.py
@@ -1,27 +1,38 @@
 import sys
 import re
 from subprocess import Popen, PIPE, STDOUT
-    #block level
-#hook -> replace with @logosformathook with ; at the end
-#end -> replace with @logosformatend with ; at the end
-#property -> replace with @logosformatproperty with NO ; at the end. Special case for block level
-#new -> replce with @logosformatnew with ; at the end
-#group -> replace with @logosformatgroup with ; at the end
-#subclass -> replace with @logosformatsubclass with ; at the end
-    #top level
-#config -> replace with @logosformatconfig 
-#hookf -> replace with @logosformathookf
-#ctor -> replace with @logosformatctor
-#dtor -> replace with @logosformatdtor
 
-    #function level
-#init -> replace with @logosformatinit
-#c -> replace with @logosformatc
-#orig -> replace with @logosformatorig
-#log -> replace with @logosformatlog
+# block level
+# hook -> replace with @logosformathook with ; at the end
+# end -> replace with @logosformatend with ; at the end
+# property -> replace with @logosformatproperty with NO ; at the end. Special case for block level
+# new -> replce with @logosformatnew with ; at the end
+# group -> replace with @logosformatgroup with ; at the end
+# subclass -> replace with @logosformatsubclass with ; at the end
+# top level
+# config -> replace with @logosformatconfig
+# hookf -> replace with @logosformathookf
+# ctor -> replace with @logosformatctor
+# dtor -> replace with @logosformatdtor
 
-specialFilterList = ["%hook","%end","%new","%group","%subclass"]
-filterList = ["%property", "%config", "%hookf", "%ctor", "%dcor", "%init", "%c", "%orig", "%log"]
+# function level
+# init -> replace with @logosformatinit
+# c -> replace with @logosformatc
+# orig -> replace with @logosformatorig
+# log -> replace with @logosformatlog
+
+specialFilterList = ["%hook", "%end", "%new", "%group", "%subclass"]
+filterList = [
+    "%property",
+    "%config",
+    "%hookf",
+    "%ctor",
+    "%dcor",
+    "%init",
+    "%c",
+    "%orig",
+    "%log",
+]
 
 
 fileContentsList = sys.stdin.read().splitlines()
@@ -30,10 +41,10 @@ newList = []
 for line in fileContentsList:
     for token in filterList:
         if token in line:
-            line = re.sub(rf"%({token[1:]})\b",r"@logosformat\1",line)
+            line = re.sub(rf"%({token[1:]})\b", r"@logosformat\1", line)
     for token in specialFilterList:
         if token in line:
-            line = re.sub(rf"%({token[1:]})\b",r"@logosformat\1",line) + ";"
+            line = re.sub(rf"%({token[1:]})\b", r"@logosformat\1", line) + ";"
     newList.append(line)
 
 command = ["clang-format"] + sys.argv[1:]
@@ -42,12 +53,11 @@ stdoutData = process.communicate(input="\n".join(newList).encode())[0]
 refinedList = stdoutData.decode().splitlines()
 
 
-
 for line in refinedList:
     if "@logosformat" in line:
-        fix = line.replace("@logosformat","%")
+        fix = line.replace("@logosformat", "%")
         if any(token in fix for token in specialFilterList):
-            print(fix[:-1]) 
+            print(fix[:-1])
         else:
             print(fix)
     else:
